30e46261da9752f8e00f9effe7fe21731f9959bf
MichelAbrantes <michel-nathan.abrantes@etu.umontpellier.fr>
2019-02-19 08:42:13 +0100

    pas fini mais presque

---
+++ src/main/java/fr/umontpellier/iut/exo1/Departement.java
@@ -0,0 +1,14 @@
+package fr.umontpellier.iut.exo1;
+
+import java.util.ArrayList;
+
+public class Departement {
+
+    private String specialite;
+    private String adresse;
+    private ArrayList etudiantsInscrits;
+
+    public Departement(String intitule, String adresse) {
+        
+    }
+}
